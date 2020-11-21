41cfcb11d472725c0ea70befd23687a2f86c14f7
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-20 21:49:52 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
-
+require "pry"
+class CashRegister
+    attr_accessor :total, :discount
+def initialize(discount = 20)
+ @total = 0
+ @discount = 20
+end
+def add_item(title, amount, quanity = 1)
+@total += amount * quanity
+end
+def apply_discount
+    @total -= @discount * @total
+    end  
+end
