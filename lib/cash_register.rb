428f2503aac6a01ac9601da9bc1844f3bf7429a5
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-21 19:25:51 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,11 +6,10 @@ class CashRegister
       @total = 0
       @discount = discount
       @items = []
-      @price = 0
+      @price = price
     end
   
     def add_item(item, price, quantity = 1)
-      @price = price
       @total += price * quantity
       if quantity > 1
         counter = 0
@@ -34,7 +33,7 @@ class CashRegister
     end
   
     def void_last_transaction
-        
+
       @total -= @price
     end
   
